a4c8c5df7153b0bc16b5dffe80dfca07e2712717
J0shuaLeong <110842480+J0shuaLeong@users.noreply.github.com>
2023-03-22 22:49:26 +0800

    changes
    updated 7seg score

--- MouseAudioDisplay/MouseAudioDisplay.srcs/sources_1/new/display_seg.v
+++ MouseAudioDisplay/MouseAudioDisplay.srcs/sources_1/new/display_seg.v
@@ -28,17 +28,33 @@ module display_seg(
     output reg dp,
     input [3:0] seg_num1, 
     input [1:0] seg_num2,
-    input [15:0] score,
-    input sw0
+    output reg [15:0] score,
+    input sw0,
+    input reset_n
     );
     
     wire clk200;
-    clock_200(CLK, clk200);
+    clk_variable clk200hz(CLK, 250000, clk200);
     
     reg [3:0] an_on = 4'b0001;
     //assign an = ~an_on;//4'b1110; //for mic
-
-    //thhh
+    
+    reg [6:0] digits [0:9]; // lookup table for digits
+    
+    // Define the digits from 0 to 9 using binary encoding
+      initial begin
+        digits[0] = 7'b1000000;
+        digits[1] = 7'b1111001;
+        digits[2] = 7'b0100100;
+        digits[3] = 7'b0110000;
+        digits[4] = 7'b0011001;
+        digits[5] = 7'b0010010;
+        digits[6] = 7'b0000010;
+        digits[7] = 7'b1111000;
+        digits[8] = 7'b0000000;
+        digits[9] = 7'b0010000;
+      end
+    
     always @(posedge CLK) begin
         an <= ~an_on;
         if (sw0 == 0) begin
@@ -83,19 +99,21 @@ module display_seg(
             end
         end else begin
             an <= 4'b0000;
-            case (score)
-                0: begin seg <= 7'b1000000; end
-                1: begin seg <= 7'b1111001; end
-                2: begin seg <= 7'b0100100; end
-                3: begin seg <= 7'b0110000; end
-                4: begin seg <= 7'b0011001; end
-                5: begin seg <= 7'b0010010; end
-                6: begin seg <= 7'b0000010; end
-                7: begin seg <= 7'b1111000; end
-                8: begin seg <= 7'b0000000; end
-                9: begin seg <= 7'b0010000; end
-                default: begin seg <= 7'b1111111; end
-            endcase
+            if (reset_n) begin
+                score <= 0;
+                seg <= digits[0];
+            end else begin
+                if (score <= 100) begin
+                    if (score % 10 == 0) begin
+                        seg <= digits[score / 10];
+                    end else begin
+                        seg <= digits[score % 10];
+                    end
+                        score <= score + 1;
+                end else begin
+                    score <= 0;
+                end
+            end           
         end
     end
     
